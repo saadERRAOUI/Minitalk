578f2d59c146148fd3665bae12edbac4fba90e2d
Saad ERRAOUI <saaderraouiinfosup@gmail.com>
2024-03-14 22:05:24 +0000

    PUSH-0.0.6 : Update minitalk_utils.c

--- minitalk_utils.c
+++ minitalk_utils.c
@@ -6,7 +6,7 @@
 /*   By: serraoui <serraoui@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/14 13:28:01 by serraoui          #+#    #+#             */
-/*   Updated: 2024/03/14 13:33:40 by serraoui         ###   ########.fr       */
+/*   Updated: 2024/03/14 22:04:35 by serraoui         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,4 +49,33 @@ void    ft_putstr_fd(char *s, int fd)
 			i++;
 		}
 	}
+}
+
+int	ft_atoi_check(const char *str, int *number)
+{
+	int		i;
+	int		sign;
+	size_t	n;
+
+	i = 0;
+	sign = 1;
+	(*number) = 0;
+	n = 0;
+	if (str[i] == '-' || str[i] == '+')
+	{
+		if (str[i] == '-')
+			sign = -1;
+		i++;
+	}
+	if (!ft_isdigit(str[i]))
+		return (0);
+	while (str[i])
+	{
+		if (!ft_isdigit(str[i]))
+			return (0);
+		n = n * 10 + str[i++] - '0';
+	}
+	if ((n > INT_MAX && sign == 1) || (n > 2147483648 && sign == -1))
+		return (0);
+	return ((*number) = n * sign, 1);
 }
